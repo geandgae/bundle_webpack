ffc81bb14edf4568d7d2b6bf5c17340c2d7afd82
g_e_out <g_e@daum.net>
2024-06-11 13:20:49 +0900

    save study

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,66 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: {
-    index: "./src/index.js",
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    // http://[devServer.host]:[devServer.port]/[output.publicPath]/[output.filename]
-    static: "./dist",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "development",
-    })
-  ],
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-    publicPath: "/",
-  },
-  optimization: {
-    moduleIds: 'deterministic',
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-        },
-      },
+module.exports = (env) => {
+  console.log(env);
+
+  return {
+    mode: "development",
+    entry: {
+      index: "./src/index.js",
+      print: "./src/print.js",
     },
-  },
+    devtool: "inline-source-map",
+    devServer: {
+      // http://[devServer.host]:[devServer.port]/[output.publicPath]/[output.filename]
+      static: "./dist",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "development",
+      }),
+    ],
+    output: {
+      filename: "[name].[contenthash].js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+      publicPath: "/",
+      pathinfo: false, // 경로 정보 생성 끄기(기능향상)
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i, //정규표현식 i는 대소문자 구분하지 않음
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.(js)$/i,
+          include: path.resolve(__dirname, "src"),
+          loader: "babel-loader",
+        },
+        {
+          test: /\.tsx?$/,
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                transpileOnly: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    optimization: {
+      runtimeChunk: "single",
+    },
+  };
 };
