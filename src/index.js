ee5aab1c76f8b09709eb8ef3a9dd6fc86fddc8d3
g_e_out <g_e@daum.net>
2024-06-11 13:55:36 +0900

    set8 div

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import _ from "lodash";
-// import Print from './print';
+// if (process.env.MODE_ENV !== "production") {
+//   console.log("development");
+// }
 
 function component() {
   const element = document.createElement("div");
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  // element.onclick = Print.bind(null, "Hello webpack!");
+  element.innerHTML = "Hello webpack!!!";
 
   return element;
 }
