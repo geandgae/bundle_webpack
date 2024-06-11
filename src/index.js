579996d4c80c61f60130fe594c4b5d9425158531
g_e_out <g_e@daum.net>
2024-06-11 09:29:03 +0900

    set2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import _ from "lodash";
+import "./style.css";
+import icon from "./icon.png";
 
 function components() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["hello", "world"], " ");
+  element.classList.add("hello");
+
+  const myIcon = new Image();
+  myIcon.src = icon;
+
+  element.appendChild(myIcon);
 
   return element
 }
